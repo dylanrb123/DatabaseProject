dc2eaffd6a2dbd05c39a95072d0b5fb462d17788
Dylan Bannon <drb2857@rit.edu>
2016-02-08 13:32:50 -0500

    added basic db connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,15 @@
  * Dylan Bannon <drb2857@rit.edu>
  */
 
-import static spark.Spark.*;
+//import static spark.Spark.*;
+import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+    public static void main(String[] args) throws Exception {
+        //get("/hello", (req, res) -> "Hello World");
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+        System.out.println(conn);
+        conn.close();
     }
 }
