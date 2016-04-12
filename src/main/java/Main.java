b84d0bd7ee7c340c8f2f61aec9cf233eb4c786db
Dylan Bannon <dylanrb123@gmail.com>
2016-04-11 22:43:54 -0400

    can now add movies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,16 +9,21 @@ import Models.Movie;
 import j2html.TagCreator;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import static spark.Spark.*;
 import static j2html.TagCreator.*;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.swing.text.DateFormatter;
 import java.io.FileReader;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Map;
 
 public class Main {
 
@@ -36,39 +41,90 @@ public class Main {
         genres.add("Action");
         genres.add("Romance");
 
-        List<Movie> moviesTemp;
         try {
             movieDao.addMovie(new Movie(0, "TEST: THE MOVIE", new Duration(1000000), new DateTime(1993, 12, 29, 0, 0, 0), MpaaRating.G, genres, "This is a movie about a test", "www.google.com", "www.youtube.com"));
             movieDao.addMovie(new Movie(0, "TEST2: RETURN OF THE TEST", new Duration(1000000), new DateTime(1993, 12, 29, 0, 0, 0), MpaaRating.G, genres, "This is a movie about a test", "www.google.com", "www.youtube.com"));
-            moviesTemp = movieDao.getAllMovies();
         } catch (SQLException e) {
             e.printStackTrace();
-            moviesTemp = new ArrayList<>();
         }
 
-        final List<Movie> movies = moviesTemp;
 
-        get("/movies", (req, res) ->
-                body().with(
-                        h1("All Movies"),
-                        div().with(
-                                movies.stream().map((movie) ->
-                                        div().with(
-                                                h2(movie.getMovieName()),
-                                                h3(Integer.toString(movie.getMovieID())),
-                                                p(movie.getSummary()),
-                                                h3("Genres: "),
-                                                ul().with(movie.getGenres().stream().map(TagCreator::li).collect(Collectors.toList())
-                                                )
-                                        )
-                                ).collect(Collectors.toList())
-                        )
-                ).render()
+        get("/movies", (req, res) -> {
+                    List<Movie> moviesTemp;
+                    try {
+                        moviesTemp = movieDao.getAllMovies();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                        moviesTemp = new ArrayList<>();
+                    }
+                    final List<Movie> movies = moviesTemp;
+                    return body().with(
+                            h1("All Movies"),
+                            div().with(
+                                    movies.stream().map((movie) ->
+                                            div().with(
+                                                    h2(movie.getMovieName()),
+                                                    h3(Integer.toString(movie.getMovieID())),
+                                                    p(movie.getSummary()),
+                                                    h3("Genres: "),
+                                                    ul().with(movie.getGenres().stream().map(TagCreator::li).collect(Collectors.toList())
+                                                    )
+                                            )
+                                    ).collect(Collectors.toList())
+                            ),
+                            h2("Add movie"),
+                            form().withMethod("post").withAction("/add-movie").withId("movie-form").with(
+                                    input().withType("text").withName("movie-name").withPlaceholder("Title").isRequired(),
+                                    input().withType("date").withName("movie-release").withPlaceholder("Release Date").isRequired(),
+                                    input().withType("text").withName("movie-genres").withPlaceholder("Genres (comma sep)").isRequired(),
+                                    select().withName("movie-rating").with(
+                                            Arrays.asList(MpaaRating.values()).stream().map(genre ->
+                                                    div().with(
+                                                            option().withValue(genre.toString()).withText(genre.toString())
+                                                    )
+                                            ).collect(Collectors.toList())
+                                    ),
+                                    input().withType("time").withName("movie-runtime").withPlaceholder("Runtime").isRequired(),
+                                    input().withType("url").withName("movie-trailer-url").withPlaceholder("Trailer URL").isRequired(),
+                                    input().withType("url").withName("movie-poster-url").withPlaceholder("Poster URL").isRequired(),
+                                    textarea().withName("movie-summary").withName("movie-summary").withPlaceholder("Summary").attr("form", "movie-form").isRequired(),
+                                    input().withType("submit").withValue("Submit")
+                            )
+                    ).render();
+                }
         );
         get("/", (req, res) ->
                 body().with(
                         a("All movies").withHref("/movies")
                 )
+        );
+        post("/add-movie", (req, res) -> {
+            String movieName = req.queryParams("movie-name");
+            String runtimeString = req.queryParams("movie-runtime");
+            String releaseDateString = req.queryParams("movie-release");
+            String ratingString = req.queryParams("movie-rating");
+            String genresString = req.queryParams("movie-genres");
+            String summary = req.queryParams("movie-summary");
+            String trailerUrl = req.queryParams("movie-trailer-url");
+            String posterUrl = req.queryParams("movie-poster-url");
+
+            DateTimeFormatter fmt = DateTimeFormat.forPattern("HH:mm");
+            Duration runtime = new Duration(fmt.parseMillis(runtimeString));
+
+            fmt = DateTimeFormat.forPattern("yyyy-MM-dd");
+            DateTime releaseDate = fmt.parseDateTime(releaseDateString);
+
+            List<String> genresList = Arrays.asList(genresString.split(","));
+
+            MpaaRating rating = MpaaRating.valueOf(ratingString);
+
+            Movie newMovie = new Movie(0, movieName, runtime, releaseDate, rating, genresList, summary, trailerUrl, posterUrl);
+            movieDao.addMovie(newMovie);
+
+            res.status(200);
+            res.redirect("/movies");
+            return res;
+        }
         );
 
 
