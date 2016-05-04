e3053b6d1922ff4db139ef6e77eeaa1879abc069
Dylan Bannon <dylanrb123@gmail.com>
2016-05-03 23:00:46 -0400

    some youtube fuckery

--- src/main/web-app/Movie.jsx
+++ src/main/web-app/Movie.jsx
@@ -1,29 +1,18 @@
 var React = require('react');
 var YouTube = require('react-youtube').default;
-
-
 module.exports = React.createClass({
     render: function() {
-        const opts = {
-            height: '390',
-            width: '640',
-            playerVars: {
-                autoplay: 1
-            }
-        };
+
         return (
             <div className="movie">
                 <h2 className="movieTitle">
                     {this.props.movieName}
                 </h2>
-                <span>
+                <div>
                     {this.props.children.toString()}
-                </span>
-                <YouTube videoId="eOrNdBpGMv8" opts={opts} onReady={this._onReady} />
+                </div>
+                <YouTube videoId="eOrNdBpGMv8" />
             </div>
         );
-    },
-    _onReady: function(e) {
-        e.target.pauseVideo();
     }
 });
--- src/main/web-app/webpack.config.js
+++ src/main/web-app/webpack.config.js
@@ -12,11 +12,13 @@ module.exports = {
     },
     module: {
         loaders: [
+            
             {
                 //tell webpack to use jsx-loader for all *.jsx files
                 test: /\.jsx$/,
                 loader: 'jsx-loader?insertPragma=React.DOM&harmony'
             }
+
         ]
     },
     externals: {
